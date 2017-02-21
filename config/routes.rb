8158411db207d21364d3d59e8423eb81ff872f81
Jorge <jorge.tll@hotmail.com>
2017-02-20 20:50:40 -0600

    Cambios Finales

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@
 
   root 'welcome#index'
   get 'mypins' => 'pins#mypins'
-  get 'pinsof/user_id' => 'pins#pinsof', :as => "pinsof"
+  get 'pinsof/:user_id' => 'pins#pinsof', :as => "pinsof"
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
